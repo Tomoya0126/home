5438991a942610a1cfd209c0bb2edc512f719bfd
Tomoya0126 <74522816+Tomoya0126@users.noreply.github.com>
2021-10-06 14:30:33 +0900

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+(function() {
+    //btnxxx‚ÍŠeƒQ[ƒ€‚Ö”ò‚Ôƒ{ƒ^ƒ“‚Ì—v‘f
+    //‘«‚µZ
+    const btnTashi = document.getElementById("btnTashi");
+    //‚©‚¯Z
+    const btnKake = document.getElementById("btnKake");
+    //ˆø‚«Z
+    const btnHiki = document.getElementById("btnHiki");
+    //‚í‚èZ
+    const btnWari = document.getElementById("btnWari");
+})()
